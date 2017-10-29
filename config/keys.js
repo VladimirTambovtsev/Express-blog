fc0a54bebfcf9789b89c306bef5710bffa2d9cb3
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-30 00:41:42 +0300

    handled oauth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://Admin:1234567890@ds241065.mlab.com:41065/stroybooks-dev',
+	googleClientID: '961173837111-ksr3gdbrhhkksg8af9js8amq7b51vjs0.apps.googleusercontent.com',
+	googleClientSecret: 'W28VkHmkdX3YBu7K0zi5cpoU'
+};
