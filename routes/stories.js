39c8fdac3c5fb8a362eca8d00107a8dafca87069
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-31 03:17:23 +0300

    Private roots debugged

--- routes/stories.js
+++ routes/stories.js
@@ -1,59 +1,71 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');  // for private folders
 const mongoose = require('mongoose');
 const Story = mongoose.model('stories');
 const User = mongoose.model('users');
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 // Stories Index
 router.get('/', (req, res) => {
-  Story.find({status: 'public'})
-  		.populate('user')
-  		.sort({date: 'desc'})
-  		.then(stories => {
-  			 res.render('stories/index', {
-  			 	stories
-  			 });
-  		});
-  
+  Story.find({status:'public'})
+    .populate('user')
+    .sort({date:'desc'})
+    .then(stories => {
+      res.render('stories/index', {
+        stories: stories
+      });
+    });
 });
 
 // Show Single Story
 router.get('/show/:id', (req, res) => {
-	Story.findOne({
-		_id: req.params.id
-	})
-	.populate('user')
-	.populate('comments.commentUser')
-	.then(story => {
-		res.render('stories/show', {
-			story
-		});
-	});
+  Story.findOne({
+    _id: req.params.id
+  })
+  .populate('user')
+  .populate('comments.commentUser')
+  .then(story => {
+    if(story.status == 'public'){
+      res.render('stories/show', {
+        story:story
+      });
+    } else {
+      if(req.user){
+        if(req.user.id == story.user._id){
+          res.render('stories/show', {
+            story:story
+          });
+        } else {
+          res.redirect('/stories');
+        }
+      } else {
+        res.redirect('/stories');
+      }
+    }
+  });
 });
 
 // List stories from a user
 router.get('/user/:userId', (req, res) => {
-	Story.find({user: req.params.userId, status: 'public'})
-			.populate('user')
-			.then(stories => {
-				res.render('stories/index', {
-					stories
-				});
-			})
+  Story.find({user: req.params.userId, status: 'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories:stories
+      });
+    });
 });
 
 // Logged in users stories
 router.get('/my', ensureAuthenticated, (req, res) => {
-	Story.find({user: req.user.id})
-			.populate('user')
-			.then(stories => {
-				res.render('stories/index', {
-					stories
-				});
-			})
+  Story.find({user: req.user.id})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories:stories
+      });
+    });
 });
-
 
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
@@ -62,100 +74,100 @@ router.get('/add', ensureAuthenticated, (req, res) => {
 
 // Edit Story Form
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
-	Story.findOne({
-		_id: req.params.id
-	})
-	.then(story => {
-		if (story.user !== req.user.id) {
-			res.redirect('/stories');
-		} else {
-			res.render('stories/edit', {
-				story
-			});
-		}
-	});
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    if(story.user != req.user.id){
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story: story
+      });
+    }
+  });
 });
 
-// Process add story
+// Process Add Story
 router.post('/', (req, res) => {
-	let allowComments;
+  let allowComments;
 
-	if (req.body.allowComments) {
-		allowComments = true;
-	} else {
-		allowComments = false;
-	}
+  if(req.body.allowComments){
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
 
-	const newStory = {
-		title: req.body.title,
-		body: req.body.body,
-		status: req.body.status,
-		allowComments: allowComments,
-		user: req.user.id
-	}
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments:allowComments,
+    user: req.user.id
+  }
 
-	// Create story 
-	new Story(newStory)
-		.save()
-		.then( story => {
-			res.redirect(`/stories/show/${story.id}`);
-		});
+  // Create Story
+  new Story(newStory)
+    .save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
 });
 
-//Edit Form 
+// Edit Form Process
 router.put('/:id', (req, res) => {
-	Story.findOne({
-		_id: req.params.id
-	})
-	.then(story => {
-		let allowComments;
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    let allowComments;
+    
+    if(req.body.allowComments){
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
 
-		if (req.body.allowComments) {
-			allowComments = true;
-		} else {
-			allowComments = false;
-		}
+    // New values
+    story.title = req.body.title;
+    story.body = req.body.body;
+    story.status = req.body.status;
+    story.allowComments = allowComments;
 
-		// New values 
-		story.title = req.body.title;
-		story.body = req.body.body;
-		story.status = req.body.status;
-		story.allowComments =allowComments;
-
-		story.save()
-			.then(story => {
-				res.redirect('/dashboard');
-			});
-	});
+    story.save()
+      .then(story => {
+        res.redirect('/dashboard');
+      });
+  });
 });
 
-// DELETE story
+// Delete Story
 router.delete('/:id', (req, res) => {
-	Story.remove({_id: req.params.id})
-			.then(() => {
-				res.redirect('/dashboard');
-			})
+  Story.remove({_id: req.params.id})
+    .then(() => {
+      res.redirect('/dashboard');
+    });
 });
 
 // Add Comment
 router.post('/comment/:id', (req, res) => {
-	Story.findOne({
-		_id: req.params.id
-	})
-	.then(story => {
-		const newComment = {
-			commentBody: req.body.commentBody,
-			commentUser: req.user.id
-		}
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
 
-		// Push to comments array
-		story.comments.unshift(newComment);
+    // Add to comments array
+    story.comments.unshift(newComment);
 
-		story.save()
-				.then(story => {
-					res.redirect(`/stories/show/${story.id}`);
-				}) 
-	});
+    story.save()
+      .then(story => {
+        res.redirect(`/stories/show/${story.id}`);
+      });
+  });
 });
 
 module.exports = router;
