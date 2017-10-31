4a237ca71e26ddd33bb0d2f8dfa475f5509248a5
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-31 03:34:11 +0300

    Deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://Admin:1234567890@ds241065.mlab.com:41065/stroybooks-dev',
-  googleClientID:'57835908856-q8m8ql9als7ee3fljrjumd477r5bjukt.apps.googleusercontent.com',
-  googleClientSecret:'OWPjmzPKmledIkj4DUr7xl60'
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
 }
